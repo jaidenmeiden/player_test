96d0ffaaf6f3f9f5945b299db4ee643f29ee829b
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-12-06 12:08:23 +0100

    feat: Finish targe script

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float health = 3f;
+
+    public void DamageReceived(float damageAmount)
     {
-        
+        health -= damageAmount;
+        if (health <= 0)
+        {
+            Die();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Die()
     {
-        
+        Destroy(gameObject);
     }
 }
